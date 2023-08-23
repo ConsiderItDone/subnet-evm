adc314970c14f47c4e9f68d53c6a899fff7a19cd
n0cte <ilnur.a.g@gmail.com>
2023-08-23 15:38:34 +0400

    add additional tests

--- contracts/test/ics20_bank_transfer_app.ts
+++ contracts/test/ics20_bank_transfer_app.ts
@@ -3,11 +3,43 @@ import { ethers } from "hardhat"
 import { Contract } from "ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
+function encodeFungibleTokenPacketData(
+  denom: string,
+  amount: string,
+  sender: string,
+  receiver: string,
+  memo: string
+): string {
+  const codec = new ethers.utils.AbiCoder();
+  return codec.encode(
+    ["tuple(string, uint256, string, address, string)"],
+    [[denom, ethers.BigNumber.from(amount), sender, receiver, memo]]
+  );
+}
+
+function makeOnRecvPacketData(data?: object): object {
+  const def = {
+    sequence: "0",
+    sourcePort: "",
+    sourceChannel: "",
+    destinationPort: "",
+    destinationChannel: "",
+    data: "0x00",
+    timeoutHeight: {
+      revisionNumber: "0",
+      revisionHeight: "0",
+    },
+    timeoutTimestamp: "0",
+  };
+  return { ...def, ...(data ?? {}) }
+}
+
 describe("ICS20BankTransferApp", function () {
   let ics20Bank: Contract;
   let ics20app: Contract;
   let owner: SignerWithAddress;
   let user: SignerWithAddress;
+
 
   before("deploy", async function () {
     const [[_owner, _user], ICS20Bank, ICS20BankTransferApp] = await Promise.all([
@@ -35,79 +67,73 @@ describe("ICS20BankTransferApp", function () {
   });
 
   it("can call only ibc", async function () {
-    const tx = ics20app.connect(user).OnRecvPacket(
-      {
-        sequence: "0",
-        sourcePort: "",
-        sourceChannel: "",
-        destinationPort: "",
-        destinationChannel: "",
-        data: "0x00",
-        timeoutHeight: {
-          revisionNumber: "0",
-          revisionHeight: "0",
-        },
-        timeoutTimestamp: "0",
-      },
-      "0x00"
-    );
-    await expect(tx).revertedWith("BaseFungibleTokenApp: caller is not the IBC contract")
+    await expect(ics20app.connect(user).OnRecvPacket(makeOnRecvPacketData(), "0x00"))
+      .revertedWith("BaseFungibleTokenApp: caller is not the IBC contract");
   });
 
-  it("success mint", async function () {
-    const codec = new ethers.utils.AbiCoder();
-    const data = codec.encode(
-      ["tuple(string, uint256, string, address, string)"],
-      [["ETH", ethers.BigNumber.from("1000"), owner.address, user.address, "some memo"]],
-    )
-
-    const onRecvPacketCall = ics20app.OnRecvPacket(
-      {
-        sequence: "0",
-        sourcePort: "port",
-        sourceChannel: "channel",
-        destinationPort: "",
-        destinationChannel: "",
-        data: data,
-        timeoutHeight: {
-          revisionNumber: "0",
-          revisionHeight: "0",
-        },
-        timeoutTimestamp: "0",
-      },
-      "0x00"
-    );
-
-    await expect(onRecvPacketCall)
+  it("mint success", async function () {
+    const data = makeOnRecvPacketData({
+      sourcePort: "port",
+      sourceChannel: "channel",
+      data: encodeFungibleTokenPacketData(
+        "ETH",
+        "1000",
+        owner.address,
+        user.address,
+        "some memo"
+      ),
+    });
+    await expect(ics20app.OnRecvPacket(data, "0x00"))
       .to.emit(ics20Bank, "Transfer")
       .withArgs(ethers.constants.AddressZero, user.address, ethers.BigNumber.from("1000"));
   });
 
-  it("success transfer", async function () {
-    const codec = new ethers.utils.AbiCoder();
-    const data = codec.encode(
-      ["tuple(string, uint256, string, address, string)"],
-      [["port/channel/ETH", ethers.BigNumber.from("1000"), user.address, owner.address, "some memo"]],
-    )
+  it("transfer channel doesn't have address", async function () {
+    const data = makeOnRecvPacketData({
+      sourcePort: "port",
+      sourceChannel: "channel",
+      data: encodeFungibleTokenPacketData(
+        "port/channel/ETH",
+        "1000",
+        user.address,
+        owner.address,
+        "some memo"
+      ),
+    });
+    await expect(ics20app.OnRecvPacket(data, "0x00")).to.be.reverted;
+  });
+
+  it("transfer escrow doesn't have balance", async function () {
+    const data = makeOnRecvPacketData({
+      sourcePort: "port",
+      sourceChannel: "channel",
+      data: encodeFungibleTokenPacketData(
+        "port/channel/BTC",
+        "1000",
+        user.address,
+        owner.address,
+        "some memo"
+      ),
+    });
     await expect(ics20app.setChannelEscrowAddresses("channel", user.address)).not.reverted;
-    await expect(ics20Bank.mint(user.address,  "port/channel/ETH", 1000)).not.reverted;
-    const onRecvPacketCall = ics20app.OnRecvPacket(
-      {
-        sequence: "0",
-        sourcePort: "port",
-        sourceChannel: "channel",
-        destinationPort: "",
-        destinationChannel: "",
-        data: data,
-        timeoutHeight: {
-          revisionNumber: "0",
-          revisionHeight: "0",
-        },
-        timeoutTimestamp: "0",
-      },
-      "0x00"
-    );
-    await expect(onRecvPacketCall)
+    const tx = await ics20app.OnRecvPacket(data, "0x00");
+    expect(await tx.wait()).to.deep.include({ logs: [] });
+  });
+
+  it("transfer success", async function () {
+    const data = makeOnRecvPacketData({
+      sourcePort: "port",
+      sourceChannel: "channel",
+      data: encodeFungibleTokenPacketData(
+        "port/channel/ETH",
+        "1000",
+        user.address,
+        owner.address,
+        "some memo"
+      ),
+    });
+    await expect(ics20Bank.mint(user.address, "port/channel/ETH", 1000)).not.reverted;
+    await expect(ics20app.OnRecvPacket(data, "0x00"))
       .to.emit(ics20Bank, "Transfer")
       .withArgs(user.address, owner.address, ethers.BigNumber.from("1000"));
   });
