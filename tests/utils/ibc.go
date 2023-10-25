4812bdea71a5ae576bd46cda58cff67a1dbb0ef2
n0cte <ilnur.a.g@gmail.com>
2023-10-25 13:44:16 +0400

    fix gas calculation in recv function

--- tests/utils/ibc.go
+++ tests/utils/ibc.go
@@ -524,6 +524,7 @@ func RunTestIbcRecvPacket(t *testing.T) {
 	_, err = waitForReceiptAndGet(ctx, ethClient, setEscrowAddrTx)
 	require.NoError(t, err)
 
+	auth.GasLimit = 200000
 	recvTx, err := ibcContract.RecvPacket(auth, contractBind.IIBCMsgRecvPacket{
 		Packet: contractBind.Packet{
 			Sequence:           big.NewInt(int64(sequence)),
